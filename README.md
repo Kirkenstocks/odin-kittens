10f39bd29f4cc4536399998c593c7f1012dd46f9
Kirk Meyer <kirkmeyer03@gmail.com>
2024-08-06 13:49:56 -0700

    Initial commit - CRUD functionality

---
+++ README.md
@@ -0,0 +1,7 @@
+# Odin-Kittens Project
+
+## Purpose
+I built this project to practice creating a simple Rails API.
+
+## Credits
+Lesson provided by The Odin Project, and can be found [here](https://www.theodinproject.com/lessons/ruby-on-rails-kittens-api).
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,60 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+
+  def new
+    @kitten = Kitten.new
+  end
+
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash.notice = "Your new kitten joined the litter!"
+      redirect_to @kitten
+    else
+      flash.alert = "Unable to add kitten. Fill out all fields and try again."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash.notice = "Your kitten rejoined the litter!"
+      redirect_to @kitten
+    else
+      flash.alert = "Unable to change kitten. Fill out all fields and try again."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash.notice = "Kitten removed from the litter. You monster!"
+    redirect_to root_path, status: :see_other
+  end
+
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
